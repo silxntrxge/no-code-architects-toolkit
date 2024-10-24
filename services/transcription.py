167937cbdfa9d5f3329e258859a0ffa08c3f861b
silxntrxge <gisejxh@gmail.com>
2024-10-24 19:39:33 +0200

    fuckit

--- services/transcription.py
+++ services/transcription.py
@@ -11,18 +11,25 @@ import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 import uuid
 import tempfile
+
 # Set up logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# Download necessary NLTK data
-nltk.download('punkt', quiet=True)
-nltk.download('punkt_tab', quiet=True)
-
 # Set the default local storage directory
 STORAGE_PATH = "/tmp/"
 
-def process_transcription(media_url, output_type, max_chars=56, language=None,):
+# Check and download necessary NLTK data
+nltk_data_path = os.path.join(os.getenv('HOME'), 'nltk_data')
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', quiet=True)
+
+def process_transcription(media_url, output_type, max_chars=56, language=None):
     """Transcribe media and return the transcript, SRT or ASS file path."""
     logger.info(f"Starting transcription for media URL: {media_url} with output type: {output_type}")
     input_filename = download_file(media_url, os.path.join(STORAGE_PATH, 'input_media'))
